d255a035d579a77f705f49de2bb7c3ff78186a51
Kellyphilo <kellywuphil@gmail.com>
2025-03-20 19:21:50 +0100

    prep model

---
+++ hurricane_debby/models/prep/prep_weather_daily.sql
@@ -0,0 +1,17 @@
+WITH daily_data AS (
+    SELECT * 
+    FROM {{ref('staging_weather_daily')}}
+),
+add_features AS (
+    SELECT *
+		, date_part('day', date) AS date_day 		-- number of the day of month
+		, date_part('month', date) AS date_month 	-- number of the month of year
+		, date_part('year', date) AS date_year 		-- number of year
+		, date_part('week', date) AS cw 			-- number of the week of year
+		, to_char(date, 'Month') AS month_name 	-- name of the month use 'FMmonth' to avoid white space
+		, to_char(date, 'Day') AS weekday 		-- name of the weekday
+    FROM daily_data 
+)
+SELECT *
+FROM add_more_features
+ORDER BY date
