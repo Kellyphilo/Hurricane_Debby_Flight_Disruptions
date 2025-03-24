cac12692764f0cca4057f51546533be716376a69
Kellyphilo <kellywuphil@gmail.com>
2025-03-24 10:44:47 +0100

    updated mart

---
+++ hurricane_debby/models/mart/jfk_departure.sql
@@ -0,0 +1,9 @@
+SELECT f.flight_date
+	  ,count(*) AS total_flights
+	  ,sum(f.cancelled) AS total_cancelled
+	  ,round(AVG(f.dep_delay), 2) AS avg_dep_delay
+	  ,round(AVG(f.arr_delay), 2) AS avg_arr_delay
+FROM {{ref('debby_prep_flights')}} f
+WHERE f.origin = 'JFK'
+GROUP BY f.flight_date
+ORDER BY flight_date
